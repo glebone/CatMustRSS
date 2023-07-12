1c22f9d6b51b32aa584782de8620755ceef04877
Glebone@iPadAirM1 <glebone@gmail.com>
2023-07-12 14:05:32 +0300

    Article fetching
    Added article fetching and other Skoda specific RSS reader enhancements

--- mustrss.py
+++ mustrss.py
@@ -1,5 +1,5 @@
 """
-   ^..^ CAT Soft - MustRSS - Mastodon RSS Personal Feed 
+   ^..^ CAT Soft - MastRSS - Mastodon RSS Personal Feed 
    for Skoda Infotainment System Online Services
    ------------------------ 
    11 July 2023 || glebone@gmail.com
@@ -9,6 +9,10 @@
 from feedgenerator import Rss201rev2Feed
 from mastodon import Mastodon
 import os
+import requests
+from bs4 import BeautifulSoup
+from newspaper import Article
+
 
 
 class EnclosureFeed(Rss201rev2Feed):
@@ -19,9 +23,11 @@ class EnclosureFeed(Rss201rev2Feed):
                                  u"type": "image/png"})
         handler.endElement("enclosure")
 
-class MustFeedGen:
+
+class MastFeedGen:
     def __init__(self):
         self.fg = EnclosureFeed(
+            image='https://seeklogo.com/images/S/skoda-logo-603B0DB338-seeklogo.com.png',
             title = '^..^ Mastodon Skoda Feed',
             link = 'https://mastodon.social',
             description = 'Mastodon RSS Feed for Skoda infotainment system',
@@ -31,26 +37,64 @@ class MustFeedGen:
 
 
     def first_n_words(self, sentence, n):
+        if sentence.startswith('<p>'):
+            sentence = sentence[3:]
         return ' '.join(sentence.split()[:n])
 
 
+    def fetch_article_content(self, url):
+        response = requests.get(url)
+        response.encoding = 'utf-8' 
+        article = Article('')
+        article.set_html(response.text)
+        article.parse()
+        return article.text
+    
+    def content_fetcher(self, content):
+        exclude_words = ['tags', 'statuses', 'mastodon']
+        soup = BeautifulSoup(content, 'html.parser')
+        links = soup.find_all('a')
+        print("-----------------")
+        article = ""
+        for link in links:
+            url = link.get('href')
+            if not any(word in url for word in exclude_words):
+                print(f"Toot  contains a link: {url}")
+                try:
+                    article = self.fetch_article_content(url)
+                    print(f"The main text of the article at {url} is: \n\n{article}\n")
+                except Exception as e:
+                    print(f"Failed to fetch article from {url} due to error: {e}")
+                break
+        print(article)
+        return article        
+        
     def generate_feed(self):
         for toot in self.mastodon.timeline_home():
-           print(toot)
-           if toot['reblogged']:
-               break
+           #print(toot)
+           cur_toot = toot
            if toot['sensitive']:
-               break 
-           item = {
-                'title': toot['account']['display_name'] + " - "  +  self.first_n_words(toot['content'], 7),
-                'link': toot['url'],
-                'description': toot['content'],
-                'pubdate': toot['created_at']}
+               continue
+           if toot['reblog'] != None:
+                cur_toot = toot['reblog']
            
-           if 'media_attachments' in toot:
+           item = {
+                'title': cur_toot['account']['display_name'][:5] + " - "  +  self.first_n_words(cur_toot['content'], 12),
+                'link': cur_toot['url'],
+                'pubdate': cur_toot['created_at']}
+           
+           if self.content_fetcher(cur_toot['content']) != "":
+                item['description'] = self.content_fetcher(cur_toot['content'])
+           else:
+                item['description'] = cur_toot['content']
+
+           if 'media_attachments' in cur_toot:
                 item['enclosure_url'] = "https://seeklogo.com/images/S/skoda-logo-603B0DB338-seeklogo.com.png"
-                for media in toot['media_attachments']:
-                    item['enclosure_url'] = media['preview_url']
+                for media in cur_toot['media_attachments']:
+                    if 'preview' in media and 'small' in media['preview']:
+                        item['enclosure_url'] = media['preview']['small']
+                    else:
+                        item['enclosure_url'] = media['preview_url']
            else:
                 item['enclosure_url'] = "https://seeklogo.com/images/S/skoda-logo-603B0DB338-seeklogo.com.png"
            
@@ -62,8 +106,9 @@ class MustFeedGen:
         with open('rss_feed.xml', 'w') as fp:
             fp.write(self.fg.writeString('utf-8'))
 
-        with open('rss_feed.xml', 'r') as fp:
-            print(fp.read())
+
+        #with open('rss_feed.xml', 'r') as fp:
+            #print(fp.read())
 
 
 from flask import Flask, Response
@@ -71,7 +116,7 @@ app = Flask(__name__)
 
 @app.route('/rss')
 def rss():
-    mfg = MustFeedGen()
+    mfg = MastFeedGen()
     mfg.generate_feed()
     with open('rss_feed.xml', 'r') as fp:
         data = fp.read()
