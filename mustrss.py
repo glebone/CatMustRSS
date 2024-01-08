bd529b3d236442d69065d5b29297f1a8bd6e0422
Gleb Dobzhansky @ iMac M1 <glebone@iMacM1>
2024-01-09 00:04:09 +0200

    New images with MIME types
    New image type recognition for feed, backported all small enhancements. Commit from Github desktop mac

--- mustrss.py
+++ mustrss.py
@@ -1,7 +1,7 @@
 """
-   ^..^ CAT Soft - MastRSS - Mastodon RSS Personal Feed 
+   ^..^ CAT Soft - MastRSS - Mastodon RSS Personal Feed
    for Skoda Infotainment System Online Services
-   ------------------------ 
+   ------------------------
    11 July 2023 || glebone@gmail.com
    initially started on Blhodatne
 """
@@ -10,6 +10,7 @@ from feedgenerator import Rss201rev2Feed
 from mastodon import Mastodon
 import os
 import requests
+import re
 from bs4 import BeautifulSoup
 from newspaper import Article
 
@@ -20,8 +21,9 @@ class EnclosureFeed(Rss201rev2Feed):
         super(EnclosureFeed, self).add_item_elements(handler, item)
         handler.startElement("enclosure",
                                 {u"url": item['enclosure_url'],
-                                 u"type": "image/png"})
+                                    u"type": item['enclosure_type']})
         handler.endElement("enclosure")
+
 
 
 class MastFeedGen:
@@ -37,21 +39,32 @@ class MastFeedGen:
 
 
     def first_n_words(self, sentence, n):
+        sentence = re.sub(r'#\w+', '', sentence)
         if sentence.startswith('<p>'):
             sentence = sentence[3:]
         return ' '.join(sentence.split()[:n])
 
+    def get_image_mime_type(self, file_path):
+        # Extract the extension from the file path
+        _, file_extension = os.path.splitext(file_path)
+
+        # Remove the leading period from the extension, if present
+        file_extension = file_extension[1:] if file_extension.startswith('.') else file_extension
+
+        # Return the formatted string
+        return f"image/{file_extension}"
 
     def fetch_article_content(self, url):
         response = requests.get(url)
-        response.encoding = 'utf-8' 
+        response.encoding = 'utf-8'
         article = Article('')
         article.set_html(response.text)
         article.parse()
+
         return article.text
-    
+
     def content_fetcher(self, content):
-        exclude_words = ['tags', 'statuses', 'mastodon']
+        exclude_words = ['tags', 'statuses']
         soup = BeautifulSoup(content, 'html.parser')
         links = soup.find_all('a')
         print("-----------------")
@@ -67,8 +80,8 @@ class MastFeedGen:
                     print(f"Failed to fetch article from {url} due to error: {e}")
                 break
         print(article)
-        return article        
-        
+        return article
+
     def generate_feed(self):
         for toot in self.mastodon.timeline_home():
            #print(toot)
@@ -77,12 +90,12 @@ class MastFeedGen:
                continue
            if toot['reblog'] != None:
                 cur_toot = toot['reblog']
-           
+
            item = {
                 'title': cur_toot['account']['display_name'][:5] + " - "  +  self.first_n_words(cur_toot['content'], 12),
                 'link': cur_toot['url'],
                 'pubdate': cur_toot['created_at']}
-           
+
            if self.content_fetcher(cur_toot['content']) != "":
                 item['description'] = self.content_fetcher(cur_toot['content'])
            else:
@@ -90,19 +103,23 @@ class MastFeedGen:
 
            if 'media_attachments' in cur_toot:
                 item['enclosure_url'] = "https://seeklogo.com/images/S/skoda-logo-603B0DB338-seeklogo.com.png"
+                item['enclosure_type'] = self.get_image_mime_type(item['enclosure_url'])
                 for media in cur_toot['media_attachments']:
                     if 'preview' in media and 'small' in media['preview']:
                         item['enclosure_url'] = media['preview']['small']
+                        item['enclosure_type'] = self.get_image_mime_type(item['enclosure_url'])
                     else:
                         item['enclosure_url'] = media['preview_url']
+                        item['enclosure_type'] = self.get_image_mime_type(item['enclosure_url'])
            else:
                 item['enclosure_url'] = "https://seeklogo.com/images/S/skoda-logo-603B0DB338-seeklogo.com.png"
-           
+                item['enclosure_type'] = self.get_image_mime_type(item['enclosure_url'])
+
            self.fg.add_item(**item)
 
-       
-        xml = self.fg.writeString('utf-8') 
-        
+
+        #xml = self.fg.writeString('utf-8')
+
         with open('rss_feed.xml', 'w') as fp:
             fp.write(self.fg.writeString('utf-8'))
 
